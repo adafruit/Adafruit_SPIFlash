bf88b9914b9ff161e086f19dfe12c832c4a352e8
Ladyada <limor@ladyada.net>
2011-10-06 11:55:14 -0400

    init

---
+++ SPIFlash.cpp
@@ -0,0 +1,579 @@
+#include <WProgram.h>
+#include "SPIFlash.h"
+
+#define SPIFLASH_BUFFERSIZE   (W25Q16BV_PAGESIZE)
+byte spiflash_buffer[SPIFLASH_BUFFERSIZE];
+
+/* ************* */
+/* CONSTRUCTORS  */
+/* ************* */
+SPIFlash::SPIFlash(uint8_t clk, uint8_t miso, uint8_t mosi, uint8_t ss) 
+{
+  _clk = clk;
+  _miso = miso;
+  _mosi = mosi;
+  _ss = ss;
+
+  digitalWrite(_ss, HIGH);  
+  digitalWrite(_clk, LOW);  
+  digitalWrite(_mosi, HIGH);  
+  pinMode(_ss, OUTPUT);
+  pinMode(_clk, OUTPUT);
+  pinMode(_mosi, OUTPUT);
+  pinMode(_miso, INPUT);
+}
+
+void SPIFlash::begin() {
+}
+
+/* *************** */
+/* PRIVATE METHODS */
+/* *************** */
+ 
+/**************************************************************************/
+/*! 
+    @brief Gets the value of the Read Status Register (0x05)
+
+    @return     The 8-bit value returned by the Read Status Register
+*/
+/**************************************************************************/
+uint8_t SPIFlash::readstatus()
+{
+  uint8_t status;
+
+  digitalWrite(_ss, LOW);
+  spiwrite(W25Q16BV_CMD_READSTAT1);    // Send read status 1 cmd
+  status = spiread();                  // Dummy write
+  digitalWrite(_ss, HIGH);
+
+  return status & (SPIFLASH_STAT_BUSY | SPIFLASH_STAT_WRTEN);
+}
+
+/************** low level SPI */
+
+void SPIFlash::readspidata(uint8_t* buff, uint8_t n) 
+{
+  digitalWrite(_ss, LOW);
+  delay(2); 
+  spiwrite(0x00);  // ToDo: Send read command
+
+  for (uint8_t i=0; i<n; i++) {
+    delay(1);
+    buff[i] = spiread();
+  }
+
+  digitalWrite(_ss, HIGH);
+}
+
+void SPIFlash::spiwrite(uint8_t c) 
+{
+  int8_t i;
+  // MSB first, clock low when inactive (CPOL 0), data valid on leading edge (CPHA 0) 
+  // Make sure clock starts low
+  digitalWrite(_clk, LOW);
+  for (i=7; i>=0; i--) 
+  {
+    digitalWrite(_clk, LOW);    
+    if (c & (1<<i)) 
+	{
+      digitalWrite(_mosi, HIGH);
+    } else 
+	{
+      digitalWrite(_mosi, LOW);
+    }    
+    digitalWrite(_clk, HIGH);
+  }
+  // Make sure clock ends low
+  digitalWrite(_clk, LOW);
+}
+
+uint8_t SPIFlash::spiread(void) 
+{
+  int8_t i, x;
+  x = 0;
+  // MSB first, clock low when inactive (CPOL 0), data valid on leading edge (CPHA 0) 
+  // Make sure clock starts low
+  digitalWrite(_clk, LOW);
+  for (i=7; i>=0; i--) 
+  {
+    digitalWrite(_clk, LOW);
+    if (digitalRead(_miso)) 
+	{
+      x |= (1<<i);
+    }
+    digitalWrite(_clk, HIGH);
+  }
+  // Make sure clock ends low
+  digitalWrite(_clk, LOW);
+  return x;
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Waits for the SPI flash to indicate that it is ready (not
+            busy) or until a timeout occurs.
+
+    @return An error message indicating that a timeoout occured
+            (SPIFLASH_ERROR_TIMEOUT_READY) or an OK signal to indicate that
+            the SPI flash is ready (SPIFLASH_ERROR_OK)
+*/
+/**************************************************************************/
+uint32_t SPIFlash::WaitForReady()
+{
+  uint32_t timeout = 0;
+  uint8_t status;
+
+  while ( timeout < 1000 )
+  {
+    status = readstatus() & SPIFLASH_STAT_BUSY;
+    if (status == 0)
+    {
+      break;
+    }
+    timeout++;
+  }
+  if ( timeout == 1000 )
+  {
+    // In this case, 1 equals an error so we can say "if(results) ..." 
+    return 1;
+  }
+
+  return 0;
+}
+
+/* ************** */
+/* PUBLIC METHODS */
+/* ************** */
+
+/**************************************************************************/
+/*! 
+    @brief  Prints a hexadecimal value in plain characters
+
+    @param  data      Pointer to the byte data
+    @param  numBytes  Data length in bytes
+*/
+/**************************************************************************/
+void SPIFlash::PrintHex(const byte * data, const uint32_t numBytes)
+{
+  uint32_t szPos;
+  for (szPos=0; szPos < numBytes; szPos++) 
+  {
+    Serial.print("0x");
+	// Append leading 0 for small values
+	if (data[szPos] <= 0xF)
+      Serial.print("0");
+	Serial.print(data[szPos], HEX);
+	if ((numBytes > 1) && (szPos != numBytes - 1))
+	{
+	  Serial.print(" ");
+	}
+  }
+  Serial.println("");
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Prints a hexadecimal value in plain characters, along with
+            the char equivalents in the following format:
+
+            00 00 00 00 00 00  ......
+
+    @param  data      Pointer to the byte data
+    @param  numBytes  Data length in bytes
+*/
+/**************************************************************************/
+void SPIFlash::PrintHexChar(const byte * data, const uint32_t numBytes)
+{
+  uint32_t szPos;
+  for (szPos=0; szPos < numBytes; szPos++) 
+  {
+	// Append leading 0 for small values
+	if (data[szPos] <= 0xF)
+      Serial.print("0");
+	Serial.print(data[szPos], HEX);
+	if ((numBytes > 1) && (szPos != numBytes - 1))
+	{
+	  Serial.print(" ");
+	}
+  }
+  Serial.print("  ");
+  for (szPos=0; szPos < numBytes; szPos++) 
+  {
+    if (data[szPos] <= 0x1F)
+	  Serial.print(".");
+	else
+	  Serial.print(data[szPos]);
+  }
+  Serial.println("");
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Gets the unique 64-bit ID assigned to this IC (useful for
+            security purposes to detect if the flash was changed, etc.)
+
+    @param[out] *buffer
+                Pointer to the uint8_t buffer that will store the 8 byte
+                long unique ID
+
+    @note   The unique ID is return in bit order 63..0
+*/
+/**************************************************************************/
+void SPIFlash::GetUniqueID(uint8_t *buffer)
+{
+  uint8_t i;
+
+  digitalWrite(_ss, LOW);
+  spiwrite(W25Q16BV_CMD_READUNIQUEID); // Unique ID cmd
+  spiwrite(0xFF);                      // Dummy write
+  spiwrite(0xFF);                      // Dummy write
+  spiwrite(0xFF);                      // Dummy write
+  spiwrite(0xFF);                      // Dummy write
+  // Read 8 bytes worth of data
+  for (i = 0; i < 8; i++)
+  {
+    buffer[i] = spiread();
+  }
+  digitalWrite(_ss, HIGH);
+}
+
+/**************************************************************************/
+/*! 
+    @brief Gets the 8-bit manufacturer ID and device ID for the flash
+
+    @param[out] *manufID
+                Pointer to the uint8_t that will store the manufacturer ID
+    @param[out] *deviceID
+                Pointer to the uint8_t that will store the device ID
+*/
+/**************************************************************************/
+void SPIFlash::GetManufacturerInfo (uint8_t *manufID, uint8_t *deviceID)
+{
+  // W25Q16BV_CMD_MANUFDEVID (0x90) provides both the JEDEC manufacturer
+  // ID and the device ID
+
+  digitalWrite(_ss, LOW);
+  spiwrite(W25Q16BV_CMD_MANUFDEVID); 
+  spiwrite(0x00);            // Dummy write
+  spiwrite(0x00);            // Dummy write
+  spiwrite(0x00);            // Dummy write
+  *manufID = spiread();
+  *deviceID = spiread();
+  digitalWrite(_ss, HIGH);
+}
+
+/**************************************************************************/
+/*! 
+    @brief Sets the write flag on the SPI flash, and if required puts the
+           WP pin in an appropriate state
+
+    @param[in]  enable
+                True (1) to enable writing, false (0) to disable it
+*/
+/**************************************************************************/
+void SPIFlash::WriteEnable (bool enable)
+{
+  // ToDo: Put the WP pin in an appropriate state if required
+
+  digitalWrite(_ss, LOW);
+  spiwrite(enable ? W25Q16BV_CMD_WRITEENABLE : W25Q16BV_CMD_WRITEDISABLE);
+  digitalWrite(_ss, HIGH);
+}
+
+/**************************************************************************/
+/*! 
+    @brief Reads the specified number of bytes from the supplied address.
+
+    This function will read one or more bytes starting at the supplied
+    address. Please note that bufferLength is zero-based, meaning you
+    should  supply '0' to read a single byte, '3' to read 4 bytes of data,
+    etc.
+
+    @param[in]  address
+                The 24-bit address where the read will start.
+    @param[out] *buffer
+                Pointer to the buffer that will store the read results
+    @param[in]  len
+                Length of the buffer.
+*/
+/**************************************************************************/
+uint32_t SPIFlash::ReadBuffer (uint32_t address, uint8_t *buffer, uint32_t len)
+{
+  uint32_t a, i;
+  a = i = 0;
+
+  // Make sure the address is valid
+  if (address >= W25Q16BV_MAXADDRESS)
+  {
+    return 0;
+  }
+
+  // Wait until the device is ready or a timeout occurs
+  if (WaitForReady())
+    return 0;
+
+  // Send the read data command
+  digitalWrite(_ss, LOW);
+  spiwrite(W25Q16BV_CMD_READDATA);      // 0x03
+  spiwrite((address >> 16) & 0xFF);     // address upper 8
+  spiwrite((address >> 8) & 0xFF);      // address mid 8
+  spiwrite(address & 0xFF);             // address lower 8
+  // Fill response buffer
+  for (a = address; a < address + len; a++, i++)
+  {
+    if (a > W25Q16BV_MAXADDRESS)
+    {
+      // Oops ... we're at the end of the flash memory
+	  // return bytes written up until now
+      return i;
+    }
+    buffer[i] = spiread();
+  }
+  digitalWrite(_ss, HIGH);
+
+  // Return bytes written
+  return i;
+}
+
+/**************************************************************************/
+/*! 
+    @brief Erases the contents of a single sector
+
+    @param[in]  sectorNumber
+                The sector number to erase (zero-based).
+*/
+/**************************************************************************/
+uint32_t SPIFlash::EraseSector (uint32_t sectorNumber)
+{
+  // Make sure the address is valid
+  if (sectorNumber >= W25Q16BV_SECTORS)
+  {
+    return 0;
+  }  
+
+  // Wait until the device is ready or a timeout occurs
+  if (WaitForReady())
+    return 0;
+
+  // Make sure the chip is write enabled
+  WriteEnable (1);
+
+  // Make sure the write enable latch is actually set
+  uint8_t status;
+  status = readstatus();
+  if (!(status & SPIFLASH_STAT_WRTEN))
+  {
+    // Throw a write protection error (write enable latch not set)
+    return 0;
+  }
+
+  // Send the erase sector command
+  uint32_t address = sectorNumber * W25Q16BV_SECTORSIZE;
+  digitalWrite(_ss, LOW);
+  spiwrite(W25Q16BV_CMD_SECTERASE4); 
+  spiwrite((address >> 16) & 0xFF);     // address upper 8
+  spiwrite((address >> 8) & 0xFF);      // address mid 8
+  spiwrite(address & 0xFF);             // address lower 8
+  digitalWrite(_ss, HIGH);
+
+  // Wait until the busy bit is cleared before exiting
+  // This can take up to 400ms according to the datasheet
+  while (readstatus() & SPIFLASH_STAT_BUSY);
+
+  return 1;
+}
+
+/**************************************************************************/
+/*! 
+    @brief Erases the entire flash chip
+*/
+/**************************************************************************/
+uint32_t SPIFlash::EraseChip (void)
+{
+  // Wait until the device is ready or a timeout occurs
+  if (WaitForReady())
+    return 0;
+
+  // Make sure the chip is write enabled
+  WriteEnable (1);
+
+  // Make sure the write enable latch is actually set
+  uint8_t status;
+  status = readstatus();
+  if (!(status & SPIFLASH_STAT_WRTEN))
+  {
+    // Throw a write protection error (write enable latch not set)
+    return 0;
+  }
+
+  // Send the erase chip command
+  digitalWrite(_ss, LOW);
+  spiwrite(W25Q16BV_CMD_CHIPERASE); 
+  digitalWrite(_ss, HIGH);
+
+  // Wait until the busy bit is cleared before exiting
+  // This can take up to 10 seconds according to the datasheet!
+  while (readstatus() & SPIFLASH_STAT_BUSY);
+
+  return 1;
+}
+
+/**************************************************************************/
+/*! 
+    @brief      Writes up to 256 bytes of data to the specified page.
+                
+    @note       Before writing data to a page, make sure that the 4K sector
+                containing the specific page has been erased, otherwise the
+                data will be meaningless.
+
+    @param[in]  address
+                The 24-bit address where the write will start.
+    @param[out] *buffer
+                Pointer to the buffer that will store the read results
+    @param[in]  len
+                Length of the buffer.  Valid values are from 1 to 256,
+                within the limits of the starting address and page length.
+*/
+/**************************************************************************/
+uint32_t SPIFlash::WritePage (uint32_t address, uint8_t *buffer, uint32_t len)
+{
+  uint8_t status;
+  uint32_t i;
+
+  // Make sure the address is valid
+  if (address >= W25Q16BV_MAXADDRESS)
+  {
+    return 0;
+  }
+
+  // Make sure that the supplied data is no larger than the page size
+  if (len > W25Q16BV_PAGESIZE)
+  {
+    return 0;
+  }
+
+  // Make sure that the data won't wrap around to the beginning of the sector
+  if ((address % W25Q16BV_PAGESIZE) + len > W25Q16BV_PAGESIZE)
+  {
+    // If you try to write to a page beyond the last byte, it will
+    // wrap around to the start of the page, almost certainly
+    // messing up your data
+    return 0;
+  }
+
+  // Wait until the device is ready or a timeout occurs
+  if (WaitForReady())
+    return 0;
+
+  // Make sure the chip is write enabled
+  WriteEnable (1);
+
+  // Make sure the write enable latch is actually set
+  status = readstatus();
+  if (!(status & SPIFLASH_STAT_WRTEN))
+  {
+    // Throw a write protection error (write enable latch not set)
+    return 0;
+  }
+
+  // Send page write command (0x02) plus 24-bit address
+  digitalWrite(_ss, LOW);
+  spiwrite(W25Q16BV_CMD_PAGEPROG);      // 0x02
+  spiwrite((address >> 16) & 0xFF);     // address upper 8
+  spiwrite((address >> 8) & 0xFF);      // address mid 8
+  if (len == 256)
+  {
+    // If len = 256 bytes, lower 8 bits must be 0 (see datasheet 11.2.17)
+    spiwrite(0);
+  }
+  else
+  {
+    spiwrite(address & 0xFF);           // address lower 8
+  }
+  // Transfer data
+  for (i = 0; i < len; i++)
+  {
+    spiwrite(buffer[i]);
+  }
+  // Write only occurs after the CS line is de-asserted
+  digitalWrite(_ss, HIGH);
+
+  // Wait at least 3ms (max page program time according to datasheet)
+  delay(3);
+  
+  // Wait until the device is ready or a timeout occurs
+  if (WaitForReady())
+    return 0;
+
+  return len;
+}
+
+/**************************************************************************/
+/*! 
+    @brief      Writes a continuous stream of data that will automatically
+                cross page boundaries.
+                
+    @note       Before writing data, make sure that the appropriate sectors
+                have been erased, otherwise the data will be meaningless.
+
+    @param[in]  address
+                The 24-bit address where the write will start.
+    @param[out] *buffer
+                Pointer to the buffer that will store the read results
+    @param[in]  len
+                Length of the buffer, within the limits of the starting
+                address and size of the flash device.
+*/
+/**************************************************************************/
+uint32_t SPIFlash::Write (uint32_t address, uint8_t *buffer, uint32_t len)
+{
+  uint32_t bytestowrite;
+  uint32_t bufferoffset;
+  uint32_t results;
+  uint32_t byteswritten;
+
+  // There's no point duplicating most error checks here since they will all be
+  // done in the underlying call to spiflashWritePage
+
+  // If the data is only on one page we can take a shortcut
+  if ((address % W25Q16BV_PAGESIZE) + len <= W25Q16BV_PAGESIZE)
+  {
+    // Only one page ... write and be done with it
+    return WritePage(address, buffer, len);
+  }
+
+  // Block spans multiple pages
+  byteswritten = 0;
+  bufferoffset = 0;
+  while(len)
+  {
+    // Figure out how many bytes need to be written to this page
+    bytestowrite = W25Q16BV_PAGESIZE - (address % W25Q16BV_PAGESIZE);
+    // Write the current page
+    results = WritePage(address, buffer+bufferoffset, bytestowrite);
+	byteswritten += results;
+    // Abort if we returned an error
+    if (!results)
+       return byteswritten;  // Something bad happened ... but return what we've written so far
+    // Adjust address and len, and buffer offset
+    address += bytestowrite;
+    len -= bytestowrite;
+    bufferoffset+=bytestowrite;
+    // If the next page is the last one, write it and exit
+    // otherwise stay in the the loop and keep writing
+    if (len <= W25Q16BV_PAGESIZE)
+    {
+      // Write the last frame and then quit
+      results = WritePage(address, buffer+bufferoffset, len);
+      byteswritten += results;
+      // Abort if we returned an error
+      if (!results)
+        return byteswritten;  // Something bad happened ... but return what we've written so far
+      // set len to zero to gracefully exit loop
+      len = 0;
+    }
+  }
+
+  // Return the number of bytes written
+  return byteswritten;
+}
---
+++ SPIFlash.h
@@ -0,0 +1,83 @@
+// SPIFlash library by adafruit
+// MIT license
+
+#include <WProgram.h>
+
+#define  SPIFLASH_SPI_STATREAD      0x02
+#define  SPIFLASH_SPI_DATAWRITE     0x01
+#define  SPIFLASH_SPI_DATAREAD      0x03
+#define  SPIFLASH_SPI_READY         0x01
+
+// Flash status bits
+#define  SPIFLASH_STAT_BUSY         0x01   // Erase/Write in Progress
+#define  SPIFLASH_STAT_WRTEN        0x02   // Write Enable Latch
+
+// SPI Flash Characteristics (W25Q16BV Specific)
+#define W25Q16BV_MAXADDRESS         0x1FFFFF
+#define W25Q16BV_PAGESIZE           256    // 256 bytes per programmable page
+#define W25Q16BV_PAGES              8192   // 2,097,152 Bytes / 256 bytes per page
+#define W25Q16BV_SECTORSIZE         4096   // 1 erase sector = 4096 bytes
+#define W25Q16BV_SECTORS            512    // 2,097,152 Bytes / 4096 bytes per sector
+#define W25Q16BV_MANUFACTURERID     0xEF   // Used to validate read data
+#define W25Q16BV_DEVICEID           0x14   // Used to validate read data
+
+// Erase/Program Instructions
+#define W25Q16BV_CMD_WRITEENABLE    0x06   // Write Enabled
+#define W25Q16BV_CMD_WRITEDISABLE   0x04   // Write Disabled
+#define W25Q16BV_CMD_READSTAT1      0x05   // Read Status Register 1
+#define W25Q16BV_CMD_READSTAT2      0x35   // Read Status Register 2
+#define W25Q16BV_CMD_WRITESTAT      0x01   // Write Status Register
+#define W25Q16BV_CMD_PAGEPROG       0x02   // Page Program
+#define W25Q16BV_CMD_QUADPAGEPROG   0x32   // Quad Page Program
+#define W25Q16BV_CMD_SECTERASE4     0x20   // Sector Erase (4KB)
+#define W25Q16BV_CMD_BLOCKERASE32   0x52   // Block Erase (32KB)
+#define W25Q16BV_CMD_BLOCKERASE64   0xD8   // Block Erase (64KB)
+#define W25Q16BV_CMD_CHIPERASE      0x60   // Chip Erase
+#define W25Q16BV_CMD_ERASESUSPEND   0x75   // Erase Suspend
+#define W25Q16BV_CMD_ERASERESUME    0x7A   // Erase Resume
+#define W25Q16BV_CMD_POWERDOWN      0xB9   // Power Down
+#define W25Q16BV_CMD_CRMR           0xFF   // Continuous Read Mode Reset
+// Read Instructions
+#define W25Q16BV_CMD_READDATA       0x03   // Read Data
+#define W25Q16BV_CMD_FREAD          0x0B   // Fast Read
+#define W25Q16BV_CMD_FREADDUALOUT   0x3B   // Fast Read Dual Output
+#define W25Q16BV_CMD_FREADDUALIO    0xBB   // Fast Read Dual I/O
+#define W25Q16BV_CMD_FREADQUADOUT   0x6B   // Fast Read Quad Output
+#define W25Q16BV_CMD_FREADQUADIO    0xEB   // Fast Read Quad I/O
+#define W25Q16BV_CMD_WREADQUADIO    0xE7   // Word Read Quad I/O
+#define W25Q16BV_CMD_OWREADQUADIO   0xE3   // Octal Word Read Quad I/O
+// ID/Security Instructions
+#define W25Q16BV_CMD_RPWRDDEVID     0xAB   // Release Power Down/Device ID
+#define W25Q16BV_CMD_MANUFDEVID     0x90   // Manufacturer/Device ID
+#define W25Q16BV_CMD_MANUFDEVID2    0x92   // Manufacturer/Device ID by Dual I/O
+#define W25Q16BV_CMD_MANUFDEVID4    0x94   // Manufacturer/Device ID by Quad I/O
+#define W25Q16BV_CMD_JEDECID        0x9F   // JEDEC ID
+#define W25Q16BV_CMD_READUNIQUEID   0x4B   // Read Unique ID
+ 
+class SPIFlash{
+ public:
+  SPIFlash(uint8_t cs, uint8_t clk, uint8_t mosi, uint8_t miso);
+  void begin(void);
+
+  // Help functions to display formatted text
+  void PrintHex(const byte * data, const uint32_t numBytes);
+  void PrintHexChar(const byte * pbtData, const uint32_t numBytes);
+  // Flash Functions
+  void GetUniqueID(uint8_t *buffer);
+  void GetManufacturerInfo (uint8_t *manufID, uint8_t *deviceID);
+  void WriteEnable (bool enable);
+  uint32_t ReadBuffer (uint32_t address, uint8_t *buffer, uint32_t len);
+  uint32_t EraseSector (uint32_t sectorNumber);
+  uint32_t EraseChip (void);
+  uint32_t WritePage (uint32_t address, uint8_t *buffer, uint32_t len);
+  uint32_t Write (uint32_t address, uint8_t *buffer, uint32_t len);
+
+ private:
+  uint8_t _ss, _clk, _mosi, _miso;
+
+  uint8_t readstatus();
+  void readspidata(uint8_t* buff, uint8_t n);
+  void spiwrite(uint8_t c);
+  uint8_t spiread(void);
+  uint32_t WaitForReady();
+};
