1344ac1468ce3651563968fb705bbe923e3d602a
Tony DiCola <tony@tonydicola.com>
2017-04-03 18:17:21 -0700

    Add simple datalogging and file printing example.

---
+++ examples/fatfs_datalogging/fatfs_datalogging.ino
@@ -0,0 +1,94 @@
+// Adafruit SPI Flash FatFs Simple Datalogging Example
+// Author: Tony DiCola
+//
+// This is a simple dataloging example using the SPI Flash
+// FatFs library.  The example will open a file on the SPI
+// flash and append new lines of data every minute. Note that
+// you MUST have a flash chip that's formatted with a flash 
+// filesystem before running.  See the fatfs_format example 
+// to perform this formatting.
+//
+// Usage:
+// - Modify the pins and type of fatfs object in the config
+//   section below if necessary (usually not necessary).
+// - Upload this sketch to your M0 express board.
+// - Open the serial monitor at 115200 baud.  You should see the
+//   example print a message every minute when it writes a new
+//   value to the data logging file.
+#include <SPI.h>
+#include <Adafruit_SPIFlash.h>
+#include <Adafruit_SPIFlash_FatFs.h>
+
+
+// Configuration of the flash chip pins and flash fatfs object.
+// You don't normally need to change these if using a Feather/Metro
+// M0 express board.
+#define FLASH_TYPE     SPIFLASHTYPE_W25Q16BV  // Flash chip type.
+                                              // If you change this be
+                                              // sure to change the fatfs
+                                              // object type below to match.
+#define FLASH_SCK      SCK1                   // Flash chip clock pin.
+#define FLASH_MISO     MISO1                  // Flash chip MISO pin.
+#define FLASH_MOSI     MOSI1                  // Flash chip MOSI pin.
+#define FLASH_SS       SS1                    // Flash chip SS pin.
+Adafruit_SPIFlash flash(FLASH_SCK, FLASH_MISO, FLASH_MOSI, FLASH_SS);
+Adafruit_W25Q16BV_FatFs fatfs(flash);
+
+// Configuration for the datalogging file:
+#define FILE_NAME      "data.csv"
+
+
+void setup() {
+  // Initialize serial port and wait for it to open before continuing.
+  Serial.begin(115200);
+  Serial.println("Adafruit SPI Flash FatFs Simple Datalogging Example");
+  
+  // Initialize flash library and check its chip ID.
+  if (!flash.begin(FLASH_TYPE)) {
+    Serial.println("Error, failed to initialize flash chip!");
+    while(1);
+  }
+  Serial.print("Flash chip JEDEC ID: 0x"); Serial.println(flash.GetJEDECID(), HEX);
+
+  // First call begin to mount the filesystem.  Check that it returns true
+  // to make sure the filesystem was mounted.
+  if (!fatfs.begin()) {
+    Serial.println("Error, failed to mount newly formatted filesystem!");
+    Serial.println("Was the flash chip formatted with the fatfs_format example?");
+    while(1);
+  }
+  Serial.println("Mounted filesystem!");
+  
+  Serial.println("Logging data every minute...");
+}
+
+void loop() {
+  // Wait 60 seconds.
+  delay(60000L);
+
+  // Open the datalogging file for writing.  The FILE_WRITE mode will open
+  // the file for appending, i.e. it will add new data to the end of the file.
+  File dataFile = fatfs.open(FILE_NAME, FILE_WRITE);
+  // Check that the file opened successfully and write a line to it.
+  if (dataFile) {
+    // Take a new data reading from a sensor, etc.  For this example just
+    // make up a random number.
+    int reading = random(0,100);
+    // Write a line to the file.  You can use all the same print functions
+    // as if you're writing to the serial monitor.  For example to write
+    // two CSV (commas separated) values:
+    dataFile.print("Sensor #1");
+    dataFile.print(",");
+    dataFile.print(reading, DEC);
+    dataFile.println();
+    // Finally close the file when done writing.  This is smart to do to make
+    // sure all the data is written to the file.
+    dataFile.close();
+    Serial.println("Wrote new measurement to data file!");
+  }
+  else {
+    Serial.println("Failed to open data file for writing!");
+  }
+  
+  Serial.println("Trying again in 60 seconds...");
+}
