64bbe55b1d5042cd033a07c43d68460d2c809f8d
hathach <thach@tinyusb.org>
2019-06-16 12:38:57 +0700

    update fatfs format

--- examples/fatfs_format/fatfs_format.ino
+++ examples/fatfs_format/fatfs_format.ino
@@ -1,14 +1,6 @@
 // Adafruit SPI Flash FatFs Format Example
 // Author: Tony DiCola
 //
-// This will partition and format the SPI flash to have a new
-// blank FAT filesystem.  This is useful for resetting the flash
-// filesystem and erasing all the old data.  This is also a good
-// example of using the low-level FatFs library functions directly
-// instead of wrappers from this library.  See documentation on
-// FatFs functions here:
-//   http://elm-chan.org/fsw/ff/00index_e.html
-//
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // !!  NOTE: YOU WILL ERASE ALL DATA BY RUNNING THIS SKETCH!  !!
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -27,39 +19,23 @@
 //   it is finished.
 //
 #include <SPI.h>
+#include <SdFat.h>
 #include <Adafruit_SPIFlash.h>
-#include <Adafruit_SPIFlash_FatFs.h>
 
-// Include the FatFs library header to use its low level functions
-// directly.  Specifically the f_fdisk and f_mkfs functions are used
-// to partition and create the filesystem.
-#include "utility/ff.h"
-
-
-// Configuration of the flash chip pins and flash fatfs object.
-// You don't normally need to change these if using a Feather/Metro
-// M0 express board.
-#define FLASH_TYPE     SPIFLASHTYPE_W25Q16BV  // Flash chip type.
-                                              // If you change this be
-                                              // sure to change the fatfs
-                                              // object type below to match.
-
-#if defined(__SAMD51__)
-  // Alternatively you can define and use non-SPI pins, QSPI isnt on a sercom
-  Adafruit_SPIFlash flash(PIN_QSPI_SCK, PIN_QSPI_IO1, PIN_QSPI_IO0, PIN_QSPI_CS);
+#if defined(__SAMD51__) || defined(NRF52840_XXAA)
+  Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS, PIN_QSPI_IO0, PIN_QSPI_IO1, PIN_QSPI_IO2, PIN_QSPI_IO3);
 #else
   #if (SPI_INTERFACES_COUNT == 1)
-    #define FLASH_SS       SS                    // Flash chip SS pin.
-    #define FLASH_SPI_PORT SPI                   // What SPI port is Flash on?
+    Adafruit_FlashTransport_SPI flashTransport(SS0, &SPI);
   #else
-    #define FLASH_SS       SS1                    // Flash chip SS pin.
-    #define FLASH_SPI_PORT SPI1                   // What SPI port is Flash on?
+    Adafruit_FlashTransport_SPI flashTransport(SS1, &SPI1);
   #endif
-
-Adafruit_SPIFlash flash(FLASH_SS, &FLASH_SPI_PORT);     // Use hardware SPI
 #endif
 
-Adafruit_W25Q16BV_FatFs fatfs(flash);
+Adafruit_SPIFlash flash(&flashTransport);
+
+// file system object
+FatFileSystem fatfs;
 
 
 void setup() {
@@ -71,11 +47,11 @@ void setup() {
   Serial.println("Adafruit SPI Flash FatFs Format Example");
 
   // Initialize flash library and check its chip ID.
-  if (!flash.begin(FLASH_TYPE)) {
+  if (!flash.begin()) {
     Serial.println("Error, failed to initialize flash chip!");
     while(1);
   }
-  Serial.print("Flash chip JEDEC ID: 0x"); Serial.println(flash.GetJEDECID(), HEX);
+  Serial.print("Flash chip JEDEC ID: 0x"); Serial.println(flash.getJEDECID(), HEX);
 
   // Wait for user to send OK to continue.
   Serial.setTimeout(30000);  // Increase timeout to print message less frequently.
@@ -87,36 +63,20 @@ void setup() {
   }
   while (!Serial.find("OK"));
 
-  // Call fatfs activate to make it the active chip that receives low level fatfs
-  // callbacks.  This is necessary before making any manual fatfs function calls
-  // (like the f_fdisk and f_mkfs functions further below).  Be sure to call
-  // activate before you call any fatfs functions yourself!
-  fatfs.activate();
+  // Call fatfs begin and passed flash object to initialize file system
+  fatfs.begin(&flash);
 
-  // Partition the flash with 1 partition that takes the entire space.
-  Serial.println("Partitioning flash with 1 primary partition...");
-  DWORD plist[] = {100, 0, 0, 0};  // 1 primary partition with 100% of space.
-  uint8_t buf[512] = {0};          // Working buffer for f_fdisk function.
-  FRESULT r = f_fdisk(0, plist, buf);
-  if (r != FR_OK) {
-    Serial.print("Error, f_fdisk failed with error code: "); Serial.println(r, DEC);
-    while(1);
+  // Use wipe() for no dot progress indicator.
+  if ( !fatfs.wipe(&Serial) )
+  {
+    Serial.println("Failed to wipe");
+    while(1) delay(1);
   }
-  Serial.println("Partitioned flash!");
 
-  // Make filesystem.
-  Serial.println("Creating and formatting FAT filesystem (this takes ~60 seconds)...");
-  r = f_mkfs("", FM_ANY, 0, buf, sizeof(buf));
-  if (r != FR_OK) {
-    Serial.print("Error, f_mkfs failed with error code: "); Serial.println(r, DEC);
-    while(1);
-  }
-  Serial.println("Formatted flash!");
-
-  // Finally test that the filesystem can be mounted.
-  if (!fatfs.begin()) {
+  // Must reinitialize after wipe.
+  if (!fatfs.begin(&flash)) {
     Serial.println("Error, failed to mount newly formatted filesystem!");
-    while(1);
+    while(1) delay(1);
   }
 
   // Done!
