da9fdf334b0e5cd75b0c4e8fce75e5c9333a0b9d
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-10-02 11:45:09 -0700

    Fixed for CPX

--- examples/SdFat_circuitpython/SdFat_circuitpython.ino
+++ examples/SdFat_circuitpython/SdFat_circuitpython.ino
@@ -33,7 +33,7 @@
 #if defined(__SAMD51__) || defined(NRF52840_XXAA)
   Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS, PIN_QSPI_IO0, PIN_QSPI_IO1, PIN_QSPI_IO2, PIN_QSPI_IO3);
 #else
-  #if (SPI_INTERFACES_COUNT == 1)
+  #if (SPI_INTERFACES_COUNT == 1 || defined(ADAFRUIT_CIRCUITPLAYGROUND_M0))
     Adafruit_FlashTransport_SPI flashTransport(SS, &SPI);
   #else
     Adafruit_FlashTransport_SPI flashTransport(SS1, &SPI1);
