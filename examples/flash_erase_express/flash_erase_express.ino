5b57404e9d44ca2df42a2bb781e9022e66eaedb5
hathach <thach@tinyusb.org>
2020-02-03 13:26:09 +0700

    example use EXTERNAL_FLASH_USE_QSPI EXTERNAL_FLASH_USE_CS/EXTERNAL_FLASH_USE_SPI

--- examples/flash_erase_express/flash_erase_express.ino
+++ examples/flash_erase_express/flash_erase_express.ino
@@ -25,18 +25,18 @@
 #include <Adafruit_SPIFlash.h>
 #include <Adafruit_NeoPixel.h>
 
-// Configuration of the flash chip pins and flash fatfs object.
-// You don't normally need to change these if using a Feather/Metro
-// M0 express board.
+// On-board external flash (QSPI or SPI) macros should already
+// defined in your board variant if supported
+// - EXTERNAL_FLASH_USE_QSPI
+// - EXTERNAL_FLASH_USE_CS/EXTERNAL_FLASH_USE_SPI
+#if defined(EXTERNAL_FLASH_USE_QSPI)
+  Adafruit_FlashTransport_QSPI flashTransport;
 
-#if defined(__SAMD51__) || defined(NRF52840_XXAA)
-  Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS, PIN_QSPI_IO0, PIN_QSPI_IO1, PIN_QSPI_IO2, PIN_QSPI_IO3);
+#elif defined(EXTERNAL_FLASH_USE_SPI)
+  Adafruit_FlashTransport_SPI flashTransport(EXTERNAL_FLASH_USE_CS, EXTERNAL_FLASH_USE_SPI);
+
 #else
-  #if (SPI_INTERFACES_COUNT == 1 || defined(ADAFRUIT_CIRCUITPLAYGROUND_M0))
-    Adafruit_FlashTransport_SPI flashTransport(SS, &SPI);
-  #else
-    Adafruit_FlashTransport_SPI flashTransport(SS1, &SPI1);
-  #endif
+  #error No QSPI/SPI flash are defined on your board variant.h !
 #endif
 
 Adafruit_SPIFlash flash(&flashTransport);
